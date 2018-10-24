3e21b2b8850271528c6308d6e5be936fe7f6eb06
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-10-24 15:33:54 +0530

    app init with bootstrap and scss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="main">
+        <header className="header">
+          <h1 className="text-center">Todo App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
